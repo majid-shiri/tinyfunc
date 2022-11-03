f2868964ba39c8665b84fe5f550d0f580b2d2520
majid-shiri <artist.m.shiri@gmail.com>
2022-11-03 07:57:03 +0330

    add commant for function  welcome

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 __author__="Majid shiri"
 
+#function welcome user
 def hello(name):
     print(f'hello {name},Your Welcome')
 
-
+#for RUN TEST
 if __name__ == '__main__':
     hello(input('Enter your Name:'))
